15ab94d6061e99084a9b12afc7839beb20cfb417
Navnit Kumar <navnitkumar@mac.home>
2026-01-19 00:59:03 -0500

    Configs for non italic and hlsearch = true

--- nvim/.config/nvim/lua/plugins/themes/everforest.lua
+++ nvim/.config/nvim/lua/plugins/themes/everforest.lua
@@ -6,6 +6,7 @@ return {
 		vim.o.background = "dark"
 		vim.g.everforest_background = "hard"
 		vim.g.everforest_enable_italic = 0
+		vim.g.everforest_disable_italic_comment = 1
 		vim.g.everforest_better_performance = 1
 		vim.g.everforest_transparent_background = 1
 		vim.g.everforest_diagnostic_text_highlight = 0
